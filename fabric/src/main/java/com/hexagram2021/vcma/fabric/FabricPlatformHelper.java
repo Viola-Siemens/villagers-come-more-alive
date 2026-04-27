41818c27e0c35e95185046c57b48c58fe62832ab
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2026-04-27 20:29:31 +0800

    feat: 跨平台 Helper

---
+++ fabric/src/main/java/com/hexagram2021/vcma/fabric/FabricPlatformHelper.java
@@ -0,0 +1,6 @@
+package com.hexagram2021.vcma.fabric;
+
+import com.hexagram2021.vcma.services.IPlatformHelper;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+}
---
+++ forge/src/main/java/com/hexagram2021/vcma/forge/ForgePlatformHelper.java
@@ -0,0 +1,6 @@
+package com.hexagram2021.vcma.forge;
+
+import com.hexagram2021.vcma.services.IPlatformHelper;
+
+public class ForgePlatformHelper implements IPlatformHelper {
+}
