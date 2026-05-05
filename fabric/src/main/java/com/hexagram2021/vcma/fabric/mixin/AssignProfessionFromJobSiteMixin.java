e44f1fa1719cd538067f540ab2aaa840a0a469f1
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2026-05-05 20:57:14 +0800

    feat: 注入 AssignProfessionFromJobSite 避免 elder 获取 jobsite

---
+++ fabric/src/main/java/com/hexagram2021/vcma/fabric/mixin/AssignProfessionFromJobSiteMixin.java
@@ -0,0 +1,32 @@
+package com.hexagram2021.vcma.fabric.mixin;
+
+import com.hexagram2021.vcma.entity.IAgingMCAEntity;
+import com.mojang.datafixers.kinds.IdF;
+import net.minecraft.core.GlobalPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.ai.behavior.AssignProfessionFromJobSite;
+import net.minecraft.world.entity.ai.behavior.declarative.BehaviorBuilder;
+import net.minecraft.world.entity.ai.behavior.declarative.MemoryAccessor;
+import net.minecraft.world.entity.npc.Villager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * 对 MCA 村民从工作站获取职业行为（{@link AssignProfessionFromJobSite}）的 Mixin
+ * @author liudongyu
+ */
+@Mixin(AssignProfessionFromJobSite.class)
+public class AssignProfessionFromJobSiteMixin {
+	@Inject(method = "method_46890", at = @At("HEAD"), remap = false, cancellable = true)
+	private static void vcma$cancelIfElder(BehaviorBuilder.Instance<Villager> var0,
+										   MemoryAccessor<IdF.Mu, GlobalPos> potentialJobSite,
+										   MemoryAccessor<IdF.Mu, GlobalPos> jobSite,
+										   ServerLevel serverLevel, Villager villager, long ticks,
+										   CallbackInfoReturnable<Boolean> cir) {
+		if(villager instanceof IAgingMCAEntity agingEntity && agingEntity.vcma$isElder()) {
+			cir.setReturnValue(false);
+		}
+	}
+}
